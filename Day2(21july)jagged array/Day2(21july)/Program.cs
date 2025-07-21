e2f0fe6930226fd3a122d51863707189adbccaba
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-07-21 13:15:01 +0530

    Jagged array

---
+++ Day2(21july)jagged array/Day2(21july)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_21july_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // jagged array in C#
+            int[][] jaggedArray = new int[3][];
+            jaggedArray[0] = new int[] { 1, 2, 3 };
+            jaggedArray[1] = new int[] { 4, 5, 6, 7 };
+            jaggedArray[2] = new int[] { 8, 9 };
+            // Displaying the jagged array by iterating through each row
+            Console.WriteLine("Jagged Array:");
+            foreach (var row in jaggedArray)
+            {
+                Console.WriteLine(string.Join(", ", row));
+            }
+        }
+    }
+}
