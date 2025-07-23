7156d06f9305dae81bbff5347f76a93574ca53e3
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2025-07-23 09:09:45 +0530

    completed

---
+++ Day4 Assignment Ques3/CustomError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4_Assignment_Ques3
+{
+    internal class CustomError : Exception
+    {
+        // here we will define our custom error for file path not found
+        public CustomError(string message) : base(message)
+        {
+
+        }
+    }
+}
